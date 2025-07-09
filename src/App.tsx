a933b00175ea19d553ef817403de6350a54d7c56
futurGH <me@futuristick.ca>
2025-07-09 19:27:23 -0400

    Set up styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import "@react95/core/GlobalStyle";
+import "@react95/core/themes/win95.css";
+
 function App() {
-	return <p>hello there</p>;
+	return <>
+			<p>hello there</p>
+	</>
 }
 
 export default App;
